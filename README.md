c96c3a91e05c4c8fb6edb11501e47141e4ee1bd7
Zuly Vargas <zuly.vargas@mail.escuelaing.edu.co>
2022-03-30 16:14:19 -0500

    Proxy local Ok con instancias en EC2

--- README.md
+++ README.md
@@ -1,8 +1,53 @@
-#PARCIAL 02 AREP
+# PARCIAL 02 AREP
 
 
 
 ![](img/arq.png)
+
+
+
+# Desarrollo
+
+Luego de realizar las aplicaciones de Calculadora y el balanceador de carga, se debe instanciar dos maquinas EC2 y subir en ambas
+los archivos correspondientes:
+
+1. Descargar el par de llaves donde realizaremos la conexión.
+2. Subir los archivos con sftp y el comando put:
+
+
+![](img/putcalculadora.png)
+
+3. Se descomprime el archivo con unzip y se ejecuta con el comando:
+
+![](img/corriendo.png)
+
+4. Al ingresar nos muestra el resultado esperado para coseno de 0:
+
+![](img/aws1.png)
+
+
+5. Repetimos el mismo proceso para la app anterior. No olvidar configurar reglas de entrada:
+![](img/corriendo.png)
+
+![](img/aws2.png)
+
+Prueba con el proxy local:
+
+![](img/local.png)
+
+![](img/cos1.png)
+
+![](img/exp2.png)
+
+
+Se realiza una tercer instancia para el PROXY en un EC2:
+
+1. Crear y descargar las llaves.
+2. Configurar reglas de seguridad.
+3. Comprimir el archivo, usar sftp y el comando put para enviar.
+4. Installar JVM, ejecutar el servidor Proxy y conectarse:
+
+
 
 
 ## Autores
--- proxy/src/main/java/edu/escuelaing/arem/LBRoundRobin.java
+++ proxy/src/main/java/edu/escuelaing/arem/LBRoundRobin.java
@@ -6,30 +6,30 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class LBRoundRobin {
     //Srvicios
-    String services1 = "http://localhost";
-    String services2 = "http://localhost";
-
+    String services1 = "http://ec2-54-87-7-198.compute-1.amazonaws.com:4568";
+    String services2 = "http://ec2-54-80-173-136.compute-1.amazonaws.com:4567";
+    String url = services1;
     private int currentServer = 0;
-    private String[] ports = {":4567",":4567"};
+    //private String[] ports = {":4567",":4568"};
 
     public Double getValueCos(Double number) throws UnirestException {
         String url = currentServer == 0 ? services1:services2 ;
-        System.out.println("URL " + url+ports[currentServer]+"/cos/"+Double.toString(number));
-        HttpResponse<String> apiResponse = Unirest.get(url+ports[currentServer]+"/cos/"+Double.toString(number)).asString();
+        System.out.println("URL consultada" + url+"/cos/"+Double.toString(number));
+        HttpResponse<String> apiResponse = Unirest.get(url+"/cos/"+Double.toString(number)).asString();
         return Double.valueOf(apiResponse.getBody());
     }
 
     public Double getValueExp(Double number) throws UnirestException {
         String url = currentServer == 0 ? services1:services2 ;
-        System.out.println("URL " + url+ports[currentServer]+"/exp/"+Double.toString(number));
-        HttpResponse<String> apiResponse = Unirest.get(url+ports[currentServer]+"/exp/"+Double.toString(number)).asString();
+        System.out.println("URL " + url+"/exp/"+Double.toString(number));
+        HttpResponse<String> apiResponse = Unirest.get(url+"/exp/"+Double.toString(number)).asString();
         return Double.valueOf(apiResponse.getBody());
     }
     /**
      * Switches between servers.
      */
     public void changeServer(){
-        currentServer=(currentServer + 1) % ports.length;
+        currentServer=(currentServer + 1) % 2;
     }
 
 }
